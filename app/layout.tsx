d7ebf9554ca059b3988b097637108dccd8cefdad
Gu89st <sarapura08asg@gmail.com>
2026-01-19 23:59:22 -0500

    actuiconalt

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Misión Blanca",
+  icons:{
+    icon: "/Docs/Logo.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
